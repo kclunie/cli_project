fc06cb3008611df263bfbb830fb1fb659f4036d4
kclunie <40764089+kclunie@users.noreply.github.com>
2019-01-21 15:57:25 -0500

    Update cli.rb
    updated cli methods

--- lib/cli_project/cli.rb
+++ lib/cli_project/cli.rb
@@ -1,6 +1,38 @@
 class Cli_project::CLI
 
+def call
+    Cli_project::Scraper.new.make_newmovies
+    puts "Welcome to Upcoming Movies"
+    start
+  end
 
+  def start
+    print_movies
+    puts "What movie would you like more information on?"
+    input = gets.strip.to_i
 
+    movie = Cli_project::Movie.find(input.to_i)
 
+    print_movie(movie)
+
+    puts ""
+    puts "Would you like to see another movie? Enter Y or N"
+
+    input = gets.strip.downcase
+    if input == "y"
+      start
+    elsif input == "n"
+      puts "Thank you! Have a great day!"
+      exit
+    else
+      puts "I don't understand that answer."
+      start
+    end
+  end
+
+  def print_movie
+    movie.all.each.with_index do |movie, index|
+     puts "#{index}. #{movie.name}"
+    end
+    
 end
