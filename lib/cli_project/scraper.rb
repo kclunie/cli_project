aa520b92a79f2232b34df2127514801e687a30c4
kclunie <40764089+kclunie@users.noreply.github.com>
2019-01-21 13:29:21 -0500

    Update scraper.rb
    Update scrape_movies_index

--- lib/cli_project/scraper.rb
+++ lib/cli_project/scraper.rb
@@ -1,9 +1,24 @@
 class Cli_project::Scraper
 
   def get_page
-    Nokogiri::HTML(open("https://www.imdb.com/movies-coming-soon/"))
+    Nokogiri::HTML(open("https://www.imdb.com/movies-coming-soon/2019-03/?ref_=cs_dt_nx"))
   end
   
+  def scrape_movies_index
+    movies = self.get_page.css(".list_item .overview-top")
+    movies.each do |title|
+    movies.css("h4").text
+    end
+  end
+ 
+    
+      
+   # description = movies.css(".outline").text
+    
   
+  
+
+        
+  end
   
 end
