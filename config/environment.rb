11c1e75e1e97ed97cbf3d8c8d995bbbc728ec17c
kclunie <40764089+kclunie@users.noreply.github.com>
2019-01-21 21:22:07 -0500

    Create environment.rb
    created environment file

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/cli_project/scraper'
+require_relative '../lib/cli_project/movie'
+require_relative '../lib/cli_project/cli'
+require_relative '../lib/cli_project/version'
