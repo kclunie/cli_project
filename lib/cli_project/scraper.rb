36aee6d63bca59ecfea5c7731583f4aaed328681
kclunie <40764089+kclunie@users.noreply.github.com>
2019-01-21 19:00:31 -0500

    Update scraper.rb
    updated scraper methods

--- lib/cli_project/scraper.rb
+++ lib/cli_project/scraper.rb
@@ -1,19 +1,26 @@
 class Cli_project::Scraper
 
   def get_page
-    Nokogiri::HTML(open("https://www.imdb.com/movies-coming-soon/2019-03/?ref_=cs_dt_nx"))
+    page = Nokogiri::HTML(open("https://www.imdb.com/movies-coming-soon/2019-03/?ref_=cs_dt_nx"))
   end
   
   def scrape_movies_index
-    movies = self.get_page.css(".list_item .overview-top")
-    movies.each do |title|
-    movies.css("h4").text
-    end
+    movies = page.css("div.list_item").text
   end
  
-    
-      
-   # description = movies.css(".outline").text
+  
+  def movie_title
+    movies.each do |movie_card|
+      title = puts movie_card.css("td.overview-top h4 a").text
+    end
+  end
+  
+  
+    def movie_descriptions
+      movies.each do |d|
+        d.css("div.outliine").text
+      end
+    end
     
   
   
