43ed2840ac1f8b174a4a3928f48e9f93a80355f1
kclunie <40764089+kclunie@users.noreply.github.com>
2019-01-21 21:16:34 -0500

    Update movie.rb
    Set up initialize method

--- lib/cli_project/movie.rb
+++ lib/cli_project/movie.rb
@@ -1,6 +1,18 @@
 class Cli_project::Movie
   
+  attr_accessor :title, :description
   
+  @@all = []
+  
+   def initialize(title=nil, description=nil)
+    @title = title
+    @description = description
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
   
   
 end
