c03ae3ec0aa5e1db53d34ca8b5d7b019631b54ab
kclunie <40764089+kclunie@users.noreply.github.com>
2019-01-25 21:59:49 -0500

    Update cli_project.rb
    Updated Environment file

--- lib/cli_project.rb
+++ lib/cli_project.rb
@@ -1,4 +1,16 @@
+#environment file
 require "cli_project/version"
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative './lib/cli_project/scraper'
+require_relative './lib/cli_project/movie'
+require_relative './lib/cli_project/cli'
+require_relative './lib/cli_project/version'
+
+
 
 module CliProject
   class Error < StandardError; end
