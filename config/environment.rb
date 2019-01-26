6cdc7ac46793f766bbca8b198a351fe95afaabc0
kcluniesa <kcluniesa@yahoo.com>
2019-01-26 03:06:43 +0000

    folder update

--- config/environment.rb
+++
@@ -1,8 +0,0 @@
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
-
-require_relative '../lib/cli_project/scraper'
-require_relative '../lib/cli_project/movie'
-require_relative '../lib/cli_project/cli'
-require_relative '../lib/cli_project/version'
