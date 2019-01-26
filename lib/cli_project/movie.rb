1da88f475341e128a835ed25095ad23c47347a6a
kcluniesa <kcluniesa@yahoo.com>
2019-01-26 22:07:55 +0000

    added attributes to movie class

--- lib/cli_project/movie.rb
+++ lib/cli_project/movie.rb
@@ -1,12 +1,13 @@
 class CliProject::Movie
   
-  attr_accessor :title, :description
+  attr_accessor :title, :description, :director
   
   @@all = []
   
    def initialize(title=nil, description=nil)
     @title = title
     @description = description
+    @director = director
     @@all << self
   end
 
